e41006cbc6d013e3b04a552c096772dc8344e3d3
Ilnur Mameshev <imameshev@inbox.ru>
2022-03-01 16:09:40 +0300

    junit4 plugin

--- src/test/java/ru/netology/service/CashBackHackerServiceTest.java
+++ src/test/java/ru/netology/service/CashBackHackerServiceTest.java
@@ -1,8 +1,38 @@
 package ru.netology.service;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Assert;
 
 public class CashBackHackerServiceTest {
+    CashBackHackerService run = new CashBackHackerService();
 
+    @Test
+    public void shouldCalculate() {
+        int actual = run.remain(800);
+        int expected = 200;
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+   public void shouldCalculate2() {
+        int actual = run.remain(1000);
+        int expected = 0;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+   public void shouldCalculate3() {
+        int actual = run.remain(700);
+        int expected = 300;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+   public void shouldCalculate4() {
+        int actual = run.remain(100);
+        int expected = 900;
+        Assert.assertEquals(expected, actual);
+    }
 
 }
